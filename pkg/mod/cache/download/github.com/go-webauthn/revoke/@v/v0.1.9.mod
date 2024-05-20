90e5647adfc85c63063f082933739b06fcd3a3ce
qianxi66 <430362385@qq.com>
2024-05-20 21:09:41 +0800

    update tag,role,admin

--- pkg/mod/cache/download/github.com/go-webauthn/revoke/@v/v0.1.9.mod
+++
@@ -1,5 +0,0 @@
-module github.com/go-webauthn/revoke
-
-go 1.20
-
-require golang.org/x/crypto v0.6.0
--- pkg/mod/github.com/leoleoasd/gofakes3@v0.0.0-20210203155129-abef9ae90e02/internal/s3assumer/tests.go
+++
@@ -1,9 +0,0 @@
-package main
-
-var tests = []Test{
-	&S300001GetVersionAfterVersioningSuspended{},
-	&S300002DeleteNonexistentVersion{},
-	&S300003DeleteVersionFromNonexistentObject{},
-	&S300004ListVersionsWithVersionMarkerButNoKeyMarker{},
-	&S300005ListVersionsWithNeverVersionedBucket{},
-}
